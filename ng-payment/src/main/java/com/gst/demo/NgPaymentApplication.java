11ec41223c6f64a24290878a2d39ddc317d016f0
lenovo <lenovo@DESKTOP-NOFNRFI>
2024-10-05 21:26:17 +0530

    First Commit for the project with basic configuration

---
+++ ng-payment/src/main/java/com/gst/demo/NgPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.gst.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgPaymentApplication.class, args);
+	}
+
+}
