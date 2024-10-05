6c3fc54bb7152624cc956ede0c9dcfd5605fbfcc
nach1981 <34371499+nach1981@users.noreply.github.com>
2024-10-05 21:52:32 +0530

    Update NgPaymentApplication.java
    Added debug stament to check before applications start

--- ng-payment/src/main/java/com/gst/demo/NgPaymentApplication.java
+++ ng-payment/src/main/java/com/gst/demo/NgPaymentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NgPaymentApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application Before Started");
 		SpringApplication.run(NgPaymentApplication.class, args);
 		System.out.println("Application Started");
 	}
