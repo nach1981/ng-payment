88b7729cdad62a8d623af0c59c0a75ae524fee18
nach1981 <34371499+nach1981@users.noreply.github.com>
2024-10-05 21:45:41 +0530

    Update NgPaymentApplication.java
    Addess Debug Comments

--- ng-payment/src/main/java/com/gst/demo/NgPaymentApplication.java
+++ ng-payment/src/main/java/com/gst/demo/NgPaymentApplication.java
@@ -8,6 +8,7 @@ public class NgPaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NgPaymentApplication.class, args);
+		System.out.println("Application Started");
 	}
 
 }
